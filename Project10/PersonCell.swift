b6a82fffd2c638e07d3355bbca69ab72ea21644b
joanne <jojoannexo@gmail.com>
2015-06-18 17:06:37 +0530

    set up functionality

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Joanne Koong on 6/18/15.
+//  Copyright (c) 2015 joanne. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
